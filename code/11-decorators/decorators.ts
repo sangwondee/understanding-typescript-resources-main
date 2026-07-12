3f5062580e9883665e629ffee718fabe75202d90
Sangwondee <sangwondee@gmail.com>
2026-07-12 16:48:42 +0700

    End Section 11 ECMAScript Decorators

--- code/11-decorators/decorators.ts
+++ code/11-decorators/decorators.ts
@@ -1,60 +1,64 @@
-function logger<T extends new (...args: any[]) => any>(
-  target: T,
-  ctx: ClassDecoratorContext
-) {
-  console.log('logger decorator');
+
+// class decorator
+function logger<T extends new (...args: any[]) => any> (target: T, ctx: any) {
+  // Decorator function ต้องมี parameter 2 ตัว 
+  // คือ target และ ctx (Decorator context)
+  console.log('Logger decorator');
   console.log(target);
   console.log(ctx);
-
   return class extends target {
     constructor(...args: any[]) {
       super(...args);
-      console.log('class constructor');
-      console.log(this);
+      console.log('Creating instance of Person');
+      console.log(`Name: ${this.name}`);
     }
   };
 }
 
-function autobind(
-  target: (...args: any[]) => any,
-  ctx: ClassMethodDecoratorContext
-) {
-  ctx.addInitializer(function (this: any) {
+// method decorator
+function autobind(target: (...args: any[]) => any, ctx: ClassMethodDecoratorContext) {
+  ctx.addInitializer(function(this: any) {
     this[ctx.name] = this[ctx.name].bind(this);
-  });
+  })
 
-  return function (this: any) {
-    console.log('Executing original function');
+  return function(this:any) {
+    console.log(`Executing original method`);
     target.apply(this);
-  };
+  }
 }
+// field decorator
+// field decorator จะมี parameter 2 ตัว คือ target และ ctx (Decorator context)
+// target จะเป็น undefined สำหรับ field decorator เพราะ field decorator 
+// ไม่สามารถเข้าถึง instance ของ class ได้
 
 function replacer<T>(initValue: T) {
-  return function replacerDecorator(
-    target: undefined,
-    ctx: ClassFieldDecoratorContext
-  ) {
+  return function replacerDecorator(target: undefined, ctx: ClassFieldDecoratorContext) {
     console.log(target);
     console.log(ctx);
-
+    
     return (initialValue: any) => {
       console.log(initialValue);
-      return initValue;
-    };
-  };
-}
-
-@logger
-class Person {
-  @replacer('')
-  name = 'Max';
-
-  @autobind
-  greet() {
-    console.log('Hi, I am ' + this.name);
+      return initValue
+    }
   }
 }
 
-const max = new Person();
-const greet = max.greet;
+
+
+@logger
+// กำหนด Decorator ให้กับ class Person
+class Person {
+  @replacer('')
+  name = 'AA';
+
+  @autobind
+  greet() {
+    console.log(`Hi i am ${this.name}`);
+  }
+}
+
+const person = new Person();
+const greet = person.greet;
 greet();
+
+console.log(person);
